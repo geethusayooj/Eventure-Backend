c11f032416b31757765e013b9f69ce8ef2ca069f
Geethu Pandath Ramesh <geethuramesh2@gmail.com>
2024-11-27 17:23:25 +0100

    add healthcheck endpoint

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const eventRoutes = require("./event.routes");
 const bookingRoutes = require("./booking.routes");
+const mongoose = require("mongoose");
 
 
 router.get("/", (req, res, next) => {
@@ -9,6 +10,24 @@ router.get("/", (req, res, next) => {
 });
 router.use("/events", eventRoutes);
 router.use("/bookings", bookingRoutes);
+//GET /api/health
+router.get('/health', (req, res) => {
+  // send ping to prevent inactivity on mongodb atlas
+  mongoose.connection.db.admin().ping()
+    .then( () => {
+      res.status(200).json({
+        status: 'ok',
+        timestamp: new Date().toISOString()
+      });
+    })
+    .catch(err => {
+      console.error('MongoDB ping failed:', err);
+      res.status(500).json({
+        status: 'error',
+        message: 'Failed to connect to MongoDB',
+      });
+    });
+});
 
 
 module.exports = router;
